d695b8a90000e7618a83e9058caecb2cca483a25
Marc Villanueva <mvillanueva@blueskybroadcast.com>
2013-12-29 18:15:56 -0800

    proverb

--- proverb/proverb.rb
+++ proverb/proverb.rb
@@ -10,67 +10,32 @@ class Proverb
 
   def to_s
     lines = []
-    consequences = Consequence.from_list(objects_of_desire)
-    consequences.each do |consequence|
-      lines << consequence.to_s
+    chain_of_events.each do |chain|
+      lines << consequence(chain[:want], chain[:lost])
     end
-
-    lines << RootCause.new(objects_of_desire.first, qualifier).to_s
+    lines << root_cause
     lines.join("\n")
   end
 
   private
 
-  def objects_of_desire
-    @objects_of_desire
-  end
-end
-
-class RootCause
-  attr_reader :object_of_desire, :qualifier
-
-  def initialize object_of_desire, qualifier
-    @object_of_desire = object_of_desire
-    @qualifier = qualifier
-  end
-
-  def to_s
-    "And all for the want of a #{qualified_object}."
-  end
-
-  private
-
-  def qualified_object
-    "#{qualifier} #{object_of_desire}".strip
-  end
-end
-
-class Consequence
-  def self.from_list(objects)
-    consequences = []
-
-    0.upto(objects.length - 2).each do |i|
-      consequences << self.new(objects[i], objects[i + 1])
+  def chain_of_events
+    chain = []
+    0.upto(objects_of_desire.length - 2).each do |i|
+      chain << {want: objects_of_desire[i], lost: objects_of_desire[i + 1]}
     end
-    consequences
+    chain
   end
 
-  def initialize(wanted_object, lost_object)
-    @wanted_object = wanted_object
-    @lost_object = lost_object
-  end
-
-  def to_s
+  def consequence(wanted_object, lost_object)
      "For want of a #{wanted_object} the #{lost_object} was lost."
   end
 
-  private
-
-  def wanted_object
-    @wanted_object
+  def root_cause
+    "And all for the want of a #{qualified_root_object}."
   end
 
-  def lost_object
-    @lost_object
+  def qualified_root_object
+    "#{qualifier} #{objects_of_desire.first}".strip
   end
 end
