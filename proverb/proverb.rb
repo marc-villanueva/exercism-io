3e26415e244199abcb267373be952273088af29e
Marc Villanueva <mvillanueva@blueskybroadcast.com>
2014-01-02 07:42:52 -0800

    proverb refactoring

--- proverb/proverb.rb
+++ proverb/proverb.rb
@@ -7,18 +7,22 @@ class Proverb
   end
 
   def to_s
-    lines = chain_of_events.collect do |chain|
-      consequence(chain[:want], chain[:lost])
-    end
+    lines = chain_of_consequences
     lines << root_cause
     lines.join("\n")
   end
 
   private
 
-  def chain_of_events
-    objects_of_desire.each_cons(2).map do |a, b|
-      {want: a, lost: b}
+  def chain_of_consequences
+    chain_of_objects.map do |chain|
+      consequence(chain[:want], chain[:lost])
+    end
+  end
+
+  def chain_of_objects
+    objects_of_desire.each_cons(2).map do |wanted_object, lost_object|
+      {want: wanted_object, lost: lost_object}
     end
   end
 
@@ -26,11 +30,15 @@ class Proverb
      "For want of a #{wanted_object} the #{lost_object} was lost."
   end
 
+  def qualified_root_object
+    "#{qualifier} #{root_object}".strip
+  end
+
   def root_cause
     "And all for the want of a #{qualified_root_object}."
   end
 
-  def qualified_root_object
-    "#{qualifier} #{objects_of_desire.first}".strip
+  def root_object
+    objects_of_desire.first
   end
 end
