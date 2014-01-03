6228ec70cc9e1df18d799e8faf3debf5bfdff6cc
Marc Villanueva <mvillanueva@blueskybroadcast.com>
2014-01-03 07:49:43 -0800

    refactor WordCounter and Tokenizer

--- word-count/phrase.rb
+++ word-count/phrase.rb
@@ -6,65 +6,53 @@ class Phrase
   end
 
   def word_count
-    WordCounter.count(self)
+    WordCounter.build(text).to_hash
   end
 end
 
 class WordCounter
-
-  def self.count(phrase, tokenizer=Tokenizer)
+  def self.build(text, tokenizer=Tokenizer)
     counter = WordCounter.new
-    tokenizer = tokenizer.new(phrase)
+    tokenizer = tokenizer.new(text)
 
     tokenizer.tokens.each do |word|
-      counter.add_word(word)
+      counter << word
     end
 
-    counter.word_count
+    counter
   end
 
   def initialize
     @words = Hash.new { |hash, key| hash[key] = 0 }
   end
 
-  def add_word(word)
-    word_count[word] += 1
+  def <<(word)
+    words[word] += 1
   end
 
-  def word_count
+  def [](word)
+    words[word]
+  end
+
+  def to_hash
+    @words
+  end
+
+  private
+
+  def words
     @words
   end
 end
 
 class Tokenizer
-  attr_reader :phrase
+  attr_reader :text
 
-  def initialize(phrase)
-    @phrase = phrase
+  def initialize(text)
+    @text = text
   end
 
   def tokens
-    return_tokens = split_into_tokens.map {|token| sanitize(token) }
-    remove_empty_tokens(return_tokens)
+    text.scan(/[0-9a-z']+/i).map {|token| token.downcase}
   end
-
-  private
-
-  def remove_empty_tokens(tokens)
-    tokens.reject(&:empty?)
-  end
-
-  def split_into_tokens
-    phrase.text.split(/\s|,/)
-  end
-
-  def sanitize(word)
-    word.downcase!
-    remove_special_characters(word)
-  end
-
-  def remove_special_characters(word)
-    word.gsub(/[^0-9a-z']/i,'').strip
-  end
-
 end
