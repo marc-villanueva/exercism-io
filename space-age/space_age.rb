63d2383511a6f22a54de55aa4b176d754e1b55a6
Marc Villanueva <mvillanueva@blueskybroadcast.com>
2014-01-03 13:22:02 -0800

    remove calculators classes

--- space-age/space_age.rb
+++ space-age/space_age.rb
@@ -6,74 +6,33 @@ class SpaceAge
   end
 
   %w(mercury venus earth mars jupiter neptune uranus saturn).each do |planet|
-    define_method("on_#{planet}") { calculator(planet).in_years }
+    define_method("on_#{planet}") { calculate(planet) }
   end
 
   private
 
-  def calculator(planet)
-    begin
-      Object.const_get("#{planet.capitalize}AgeCalculator")
-    rescue
-      EarthAgeCalculator
-    end.new(seconds)
-  end
-end
-
-class EarthAgeCalculator
-  attr_reader :seconds
-
-  def initialize(seconds)
-    @seconds = seconds
+  def calculate(planet)
+    (seconds / seconds_in_a_year(planet)).round(2)
   end
 
-  def in_years
-    (seconds / seconds_in_a_year).round(2)
+  def seconds_in_a_year(planet)
+    seconds_in_a_year_on_earth * seconds_multiplier[planet.to_sym]
   end
 
-  def seconds_in_a_year
+  def seconds_in_a_year_on_earth
     31557600.0
   end
-end
 
-class MercuryAgeCalculator < EarthAgeCalculator
-  def seconds_in_a_year
-    super * 0.2408467
-  end
-end
-
-class VenusAgeCalculator < EarthAgeCalculator
-  def seconds_in_a_year
-    super * 0.61519726
-  end
-end
-
-class MarsAgeCalculator < EarthAgeCalculator
-  def seconds_in_a_year
-    super * 1.8808158
-  end
-end
-
-class JupiterAgeCalculator < EarthAgeCalculator
-  def seconds_in_a_year
-    super * 11.862615
-  end
-end
-
-class SaturnAgeCalculator < EarthAgeCalculator
-  def seconds_in_a_year
-    super * 29.447498
-  end
-end
-
-class UranusAgeCalculator < EarthAgeCalculator
-  def seconds_in_a_year
-    super * 84.016846
-  end
-end
-
-class NeptuneAgeCalculator < EarthAgeCalculator
-  def seconds_in_a_year
-    super * 164.79132
+  def seconds_multiplier
+    {
+      mercury: 0.2408467,
+      venus: 0.61519726,
+      earth: 1,
+      mars: 1.8808158,
+      jupiter: 11.862615,
+      neptune: 164.79132,
+      uranus: 84.016846,
+      saturn: 29.447498
+    }
   end
 end
