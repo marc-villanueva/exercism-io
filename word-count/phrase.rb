c6b7f93f4083343da80143fe0139172eb96fe3e2
Marc Villanueva <mvillanueva@blueskybroadcast.com>
2014-01-04 08:46:52 -0800

    refactor word counting to hash#reduce

--- word-count/phrase.rb
+++ word-count/phrase.rb
@@ -6,53 +6,13 @@ class Phrase
   end
 
   def word_count
-    WordCounter.build(text).to_hash
-  end
-end
-
-class WordCounter
-  def self.build(text, tokenizer=Tokenizer)
-    counter = WordCounter.new
-    tokenizer = tokenizer.new(text)
-
-    tokenizer.tokens.each do |word|
-      counter << word
+    words.reduce(Hash.new(0)) do |hash, word|
+      hash[word] += 1
+      hash
     end
-
-    counter
   end
-
-  def initialize
-    @words = Hash.new { |hash, key| hash[key] = 0 }
-  end
-
-  def <<(word)
-    words[word] += 1
-  end
-
-  def [](word)
-    words[word]
-  end
-
-  def to_hash
-    @words
-  end
-
-  private
 
   def words
-    @words
-  end
-end
-
-class Tokenizer
-  attr_reader :text
-
-  def initialize(text)
-    @text = text
-  end
-
-  def tokens
-    text.scan(/[0-9a-z']+/i).map(&:downcase)
+    text.downcase.scan(/[0-9a-z']+/)
   end
 end
