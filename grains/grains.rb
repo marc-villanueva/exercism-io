662b14747833d51100fe8abc21d98ebfc54a67e3
Marc Villanueva <mvillanueva@blueskybroadcast.com>
2014-01-11 18:45:46 -0800

    refactor grains

--- grains/grains.rb
+++ grains/grains.rb
@@ -1,21 +1,31 @@
 class Grains
+  attr_reader :total_number_of_squares
+
+  def initialize(total_number_of_squares=64)
+    @total_number_of_squares = total_number_of_squares
+  end
+
   def square(number)
-    if number == 1
-      square_totals[number] ||= 1
-    else
-      square_totals[number] ||= 2 * square(number - 1)
-    end
+    grain_calculation_cache[number] ||= calculate_number_of_grains(number)
   end
 
   def total
-    (1..64).reduce(0) do |sum, square_number|
+    (1..total_number_of_squares).reduce(0) do |sum, square_number|
       sum + square(square_number)
     end
   end
 
   private
 
-  def square_totals
-    @square_totals ||= Hash.new(nil)
+  def calculate_number_of_grains(number)
+    if number == 1
+      1
+    else
+      2 * calculate_number_of_grains(number - 1)
+    end
+  end
+
+  def grain_calculation_cache
+    @grain_calculation_cache ||= Hash.new(nil)
   end
 end
