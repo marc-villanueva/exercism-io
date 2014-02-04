5fb6aad1f6b40604a47c636d2391797e52d06c7c
Marc Villanueva <mvillanueva@blueskybroadcast.com>
2014-02-04 00:33:21 -0800

    attempt at better naming

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -3,7 +3,7 @@ class Hamming
     dna_strand1 = DnaStrand.new(sequence1)
     dna_strand2 = DnaStrand.new(sequence2)
 
-    dna_strand1.unique_pairs(dna_strand2).size
+    dna_strand1.point_mutations(dna_strand2).size
   end
 end
 
@@ -14,17 +14,17 @@ class DnaStrand
     @sequence = sequence_string.chars
   end
 
-  def unique_pairs(other_strand)
-    unique_filter(sequence_pairs(other_strand))
+  def point_mutations(other_strand)
+    non_matching_pairs((nucleotide_pairs(other_strand)))
   end
 
   private
 
-  def sequence_pairs(other_strand)
-    sequence.zip(other_strand.sequence)
+  def non_matching_pairs(pairs)
+    pairs.select {|pair| pair.compact.uniq == pair }
   end
 
-  def unique_filter(pairs)
-    pairs.select {|pair| pair.compact.uniq == pair }
+  def nucleotide_pairs(other_strand)
+    sequence.zip(other_strand.sequence)
   end
 end
